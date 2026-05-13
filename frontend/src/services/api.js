58f9e3c00aa5dc8aed4b2461b5d6dac4c877d9ec
Dheeraj12F <dheeraj015423@gmail.com>
2026-05-13 14:09:06 +0530

    added railway backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API = axios.create({
-//   baseURL: "http://127.0.0.1:8000",
-// });
-const API = "taskflow-manager-production-de61.up.railway.app";
+const API = axios.create({
+  baseURL: "https://taskflow-manager-production-de61.up.railway.app",
+});
+
 export default API;
